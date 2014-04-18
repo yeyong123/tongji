8115730aeb57ea8ac7f89dc349b65e09ecc2549b
yeyong14 <yeyong14@gmail.com>
2014-04-18 13:51:18 +0800

    修改链接

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
   attr_accessible :name
 	has_many :advisories
+
+	before_destroy :referenced_by_any_advisory
+
+	private 
+		
+	def referenced_by_any_advisory
+		if advisories.empty?
+			return true
+		else
+			errors.add(:base, "不能删除,有关联的产品")
+			return false
+		end
+	end
 end
